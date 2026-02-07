e164ee15a42d57a75c28ee29f4af0c1bc4c32d3c
aru-01 <arifu5758@gmail.com>
2026-02-07 16:26:16 +0600

    added go- variable practice

---
+++ 2nd_variable/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10                //long version (int)
+	var s string = "Hello World!" //long version (str)
+	var b float64 = 64.52         //long version (float)
+	// fmt.Println(a)
+	fmt.Println(a, s, b)
+
+	c := false // shorter version (boolean)
+	fmt.Println(c)
+
+}
