125158842ef2a0e0fe455a0227aff0572ac3df0c
aru-01 <arifu5758@gmail.com>
2026-02-12 17:06:24 +0600

    added go- condition practice

---
+++ 3rd_condition/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	tk := 500
+	sex := "male"
+
+	if tk > 50 && sex == "male" {
+		fmt.Println("You are a rich man bro")
+	} else {
+		fmt.Println("Shala goribs")
+	}
+
+}
