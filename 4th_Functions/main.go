b84ffe9d9af9b69917a98646f8d932e7119bd503
aru-01 <arifu5758@gmail.com>
2026-02-14 09:02:28 +0600

    added go- Function with Return Values and Types Practice

--- 4th_Functions/main.go
+++ 4th_Functions/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+// Print func
+/*
 func sum(a int, b int) {
 	fmt.Println(a + b)
+}
+*/
+
+// return func
+func sum_and_mult(a int, b int) (int, int) {
+	sum := a + b
+	mult := a * b
+	return sum, mult
 }
 
 func main() {
 	a := 4
 	b := 6
 
-	sum(a, b)
-	sum(324, 45245)
+	x, y := sum_and_mult(a, b)
+	fmt.Println(x, y)
+	x, y = sum_and_mult(324, 45245)
+	fmt.Println(x, y)
 }
