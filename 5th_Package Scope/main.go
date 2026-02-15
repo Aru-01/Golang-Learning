62a6b88e13aa73129649646774d93512c79d44ae
aru-01 <arifu5758@gmail.com>
2026-02-15 11:29:56 +0600

    added go- Package Scope Practice

---
+++ 5th_Package Scope/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "example.go/mathlib"
+
+func main() {
+	fmt.Println(mathlib.Add(4, 5))
+	fmt.Println(mathlib.Mult(4, 5))
+	fmt.Println(mathlib.Div(40, 5))
+
+}
---
+++ 5th_Package Scope/mathlib/math.go
@@ -0,0 +1,13 @@
+package mathlib
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Mult(a int, b int) int {
+	return a * b
+}
+
+func Div(a int, b int) int {
+	return a / b
+}
